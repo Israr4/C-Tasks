cb5a330b9fa189dad9f0ea78d500a5ef11cfd0c2
Israr4 <israrrajpoot34@gmail.com>
2023-06-23 10:58:56 +0500

    new

--- Cars_Collision/cars_collision.cpp
+++ Cars_Collision/cars_collision.cpp
@@ -8,25 +8,16 @@ class Car
     double x_, y_ ;   
     double velocity_,angle_;
 
-    Car()
-    {
-        x_        = 0;
-        y_        = 0;
-        velocity_ = 0;
-        angle_    = 0;
-    }
-
-    Car(double x, double y, double velocity, double angle)
+    explicit Car(double x, double y, double velocity, double angle)
     {
         x_        = x;
         y_        = y;
         velocity_ = velocity;
-        angle_    = angle;  
+        angle_    = angle * M_PI / 180;  
     }
-    
 };
 
-bool calculateCollision(const Car& obj1, const Car& obj2, double collisionTime)
+bool calculateCollision(const Car& obj1, const Car& obj2)
 {
     double time_x = (obj1.velocity_ * cos(obj1.angle_)) - (obj2.velocity_ * cos(obj2.angle_));
     double time_y = (obj1.velocity_ * sin(obj1.angle_)) - (obj2.velocity_ * sin(obj2.angle_));
@@ -34,12 +25,10 @@ bool calculateCollision(const Car& obj1, const Car& obj2, double collisionTime)
     double dist_X = obj2.x_ - obj1.x_;
     double dist_Y = obj2.y_ - obj1.y_;
 
-    collisionTime = ((dist_X * time_x) + (dist_Y * time_y)) / ((time_x * time_x) + (time_y * time_y));
+    double collisionTime = ((dist_X * time_x) + (dist_Y * time_y)) / ((time_x * time_x) + (time_y * time_y));
 
     if (collisionTime >= 0 && collisionTime <= 1) 
     {
-        double collision_X = obj1.x_ + obj1.velocity_ * cos(obj1.angle_) * collisionTime;
-        double collision_Y = obj1.y_ + obj1.velocity_ * sin(obj1.angle_) * collisionTime;
         return true;
     } 
     else 
@@ -51,13 +40,14 @@ bool calculateCollision(const Car& obj1, const Car& obj2, double collisionTime)
 
 int main() 
 {  
-    Car object1(200, 2, 5, 45  * M_PI / 180);    //angle in radian    
-    Car object2(55 , 4, 7, 135 * M_PI / 180);   
+    // Car object1(200, 2, 5, 45 );    //angle in radian    
+    // Car object2(55 , 4, 7, 135);   
  
-    // Car object1(2, 2, 5, 45  * M_PI / 180);    //angle in radian    
-    // Car object2(5, 4, 7, 135 * M_PI / 180);    
-    double collisionTime;
-    if(calculateCollision(object1, object2, collisionTime))
+    Car object1(2, 2, 5, 45 );    //angle in radian    
+    Car object2(5, 4, 7, 135); 
+
+
+    if(calculateCollision(object1, object2))
     {
         std::cout << "Collision detected!" << std::endl;
     }
