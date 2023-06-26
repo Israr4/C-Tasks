94e8df1a792d1b7a6b759e5f1c92c43b538c0080
Israr4 <israrrajpoot34@gmail.com>
2023-06-26 13:06:13 +0500

    build folder deleted

--- Cars_Collision/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maanz-ai/cars_collision/C-Tasks/Cars_Collision
-# Build directory: /home/maanz-ai/cars_collision/C-Tasks/Cars_Collision/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Cars_Collision/main.cpp
+++ Cars_Collision/main.cpp
@@ -2,14 +2,14 @@
 
 int main() 
 {  
-    Car object1(200, 2, 5, 45 );    //angle in radian    
-    Car object2(55 , 4, 7, 135);   
+    Car car1(200, 2, 5, 45 );    //angle in radian    
+    Car car2(55 , 4, 7, 135);   
  
-    // Car object1(2, 2, 5, 45 );    //angle in radian    
-    // Car object2(5, 4, 7, 135); 
+    // Car car1(2, 2, 5, 45 );    //angle in radian    
+    // Car car2(5, 4, 7, 135); 
 
 
-    if(isCalculatingCollision(object1, object2))
+    if(isCalculatingCollision(car1, car2))
     {
         std::cout << "Collision detected!" << std::endl;
     }
@@ -17,6 +17,5 @@ int main()
     {
         std::cout << "No collision detected." << std::endl;
     }
-
     return 0;
 }
